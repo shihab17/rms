2141c43bbb0e1e9e585dd4a1815e3bc95e33e659
shihab17 <56069831+shihab17@users.noreply.github.com>
2020-11-16 20:11:15 +0600

    change password(admin)

--- admin/view/header.php
+++ admin/view/header.php
@@ -1,7 +1,7 @@
 <center><br><nav style="font-size: 27px; background-color:#f2f2f2; " >
 <a style="text-decoration: none;" href="../view/index.php">DashBoard &nbsp;&nbsp;</a>
 <a style="text-decoration: none;" href="#">User&nbsp;&nbsp;</a>
-<a style="text-decoration: none;" href="#">Profile&nbsp;&nbsp;</a>
+<a style="text-decoration: none;" href="../view/profile.php">Profile&nbsp;&nbsp;</a>
 <a style="text-decoration: none;" href="../view/item.php">Items&nbsp;&nbsp;</a>
 <a style="text-decoration: none;" href="#">Stock&nbsp;&nbsp;</a>
 <a style="text-decoration: none;" href="#">Promotions&nbsp;&nbsp;</a>
