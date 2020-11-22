ca86610abbc8b559eeddfa5d065eccbc9a4bc352
Shahadat308 <63419245+Shahadat308@users.noreply.github.com>
2020-11-22 23:39:02 +0600

    img

---
+++ account/view/setdp.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE>
+<html>
+<head></head>
+<body>
+<h1>Set profile picture<h1>
+<img src="profileimg.jpg" alt="Profile piture" width="250" height="250">
+<form action="" method="POST" enctype="multipart/form-data" >
+<input type="file" name="setdp" required><br>
+
+<label></label><br>
+<input type="Submit" value="upload" name="submit"></td><br>
+
+</form>
+</body>
+
+
+
+
+</html>
