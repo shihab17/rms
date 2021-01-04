419e169d954d9fe23d088f1963040ca3b1d28203
Shahadat308 <63419245+Shahadat308@users.noreply.github.com>
2021-01-04 11:59:20 +0600

    update

--- account/view/changepass.php
+++ account/view/changepass.php
@@ -8,6 +8,30 @@
 	color:red;
 }
 </style>
+<script>
+	function validate() {
+		var oldPsw = document.getElementById('oldPsw').value;
+		var newPsw = document.getElementById('newPsw').value;
+		var rePsw = document.getElementById('rePsw').value;
+		// console.log(x);
+		if(oldPsw == "") {
+			document.getElementById('errorMsg').innerHTML = "oldPsw is empty";
+			document.getElementById('errorMsg').style.color = "red";
+			return false;	
+		}
+		if(newPsw == "") {
+			document.getElementById('errorMsg').innerHTML = "newPsw is empty";
+			document.getElementById('errorMsg').style.color = "red";
+			return false;	
+		}
+		if(rePsw == "") {
+			document.getElementById('errorMsg').innerHTML = "rePsw is empty";
+			document.getElementById('errorMsg').style.color = "red";
+			return false;	
+		}
+		
+	}
+</script>
 <?php include('../view/Header.php'); ?>
 <center>
 <h1>Change Password</h1>
@@ -15,7 +39,7 @@
 $oldPsw=$newPsw=$rePsw="";?>
 <span class= "success" ><?php if (isset($success)){echo $success;} ?></span><br>
 <span class="error" ><?php if(isset($error_message)){echo $error_message;} ?></span>
-<form action="" method="POST" >
+<form action="" method="POST" onsubmit=" return validate()" >
 	<label for="oldPsw" >Old Password:</label>
 	<input type="password" name="oldPsw" id="oldPsw" placeholder="Type your current Password">
 	<span class="error" ><?php if(isset($oldPswErr)){echo $oldPswErr;} ?></span>
