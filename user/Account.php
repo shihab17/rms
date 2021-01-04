a5c5932bb59621b84603deef505a6ca0a15194b7
Md. Aminul Rasel <57564771+Aminul138@users.noreply.github.com>
2021-01-04 14:33:28 +0600

    Delete Account.php

--- user/Account.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html>
-<body>
-<h1> Welcome to Our Site </h1>
-<a href="Accountlog.php"> Login your Account </a><br><br><br>
-<a href="account Reg.php">  Registration </a><br><br><br>
-</body>
-</html>
